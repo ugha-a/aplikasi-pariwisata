478610d78da90bb8d7d5d076b35b84f404ad906c
MarioKozlet <ugha289@gmail.com>
2025-08-01 02:44:36 +0800

    Implement role-based visibility for admin navigation links in sidebar

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -10,49 +10,59 @@
     <!-- Sidebar Menu -->
     <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-            <li class="nav-item">
-                <a href="{{ route('admin.dashboard') }}" class="nav-link">
-                    <i class="nav-icon fas fa-th"></i>
-                    <p>
-                        {{ __('Dashboard') }}
-                    </p>
-                </a>
-            </li>
+            @if (in_array(auth()->user()->role, ['admin', 'dinas']))
+                <li class="nav-item">
+                    <a href="{{ route('admin.dashboard') }}" class="nav-link">
+                        <i class="nav-icon fas fa-th"></i>
+                        <p>
+                            {{ __('Dashboard') }}
+                        </p>
+                    </a>
+                </li>
+            @endif
 
-            <li class="nav-item">
-                <a href="{{ route('admin.users.index') }}" class="nav-link">
-                    <i class="nav-icon fas fa-users"></i>
-                    <p>
-                        {{ __('Users') }}
-                    </p>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a href="{{ route('admin.bookings.index') }}" class="nav-link">
-                    <i class="nav-icon fas fa-book"></i>
-                    <p>
-                        {{ __('Booking') }}
-                    </p>
-                </a>
-            </li>
+            @if (in_array(auth()->user()->role, ['admin']))
+                <li class="nav-item">
+                    <a href="{{ route('admin.users.index') }}" class="nav-link">
+                        <i class="nav-icon fas fa-users"></i>
+                        <p>
+                            {{ __('Users') }}
+                        </p>
+                    </a>
+                </li>
+            @endif
+            @if (in_array(auth()->user()->role, ['admin', 'dinas']))
+                <li class="nav-item">
+                    <a href="{{ route('admin.bookings.index') }}" class="nav-link">
+                        <i class="nav-icon fas fa-book"></i>
+                        <p>
+                            {{ __('Booking') }}
+                        </p>
+                    </a>
+                </li>
+            @endif
 
-            <li class="nav-item">
-                <a href="{{ route('admin.travel_packages.index') }}" class="nav-link">
-                    <i class="nav-icon fa fa-hotel"></i>
-                    <p>
-                        {{ __('Travel Package') }}
-                    </p>
-                </a>
-            </li>
+            @if (in_array(auth()->user()->role, ['admin']))
+                <li class="nav-item">
+                    <a href="{{ route('admin.travel_packages.index') }}" class="nav-link">
+                        <i class="nav-icon fa fa-hotel"></i>
+                        <p>
+                            {{ __('Travel Package') }}
+                        </p>
+                    </a>
+                </li>
+            @endif
 
-            <li class="nav-item">
-                <a href="{{ route('admin.locations.index') }}" class="nav-link">
-                    <i class='fas fa-map-marker-alt' style='font-size:24px'></i>
-                    <p>
-                        {{ __(' Locations') }}
-                    </p>
-                </a>
-            </li>
+            @if (in_array(auth()->user()->role, ['admin']))
+                <li class="nav-item">
+                    <a href="{{ route('admin.locations.index') }}" class="nav-link">
+                        <i class='fas fa-map-marker-alt' style='font-size:24px'></i>
+                        <p>
+                            {{ __(' Locations') }}
+                        </p>
+                    </a>
+                </li>
+            @endif
 
 
         </ul>
